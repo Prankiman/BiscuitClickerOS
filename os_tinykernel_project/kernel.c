d1b3415b6daf915cca8cff5f73016ea97f8db3cd
Prankiman <kolsmyrarvid@gmail.com>
2022-08-15 23:55:46 +0200

    commit

--- os_tinykernel_project/kernel.c
+++ os_tinykernel_project/kernel.c
@@ -1,5 +1,3 @@
 int main(){
-    char* video_memory = (char*) 0xb8000;
-    *video_memory = 'X';
-    return 0;
+
 }
--- os_tinykernel_project/types.h
+++ os_tinykernel_project/types.h
@@ -4,6 +4,7 @@
 typedef unsigned int   u32;
 typedef unsigned long  u_32;
 typedef          int   s32;
+typedef          long  s_32;
 typedef unsigned short u16;
 typedef          short s16;
 typedef unsigned char  u8;
