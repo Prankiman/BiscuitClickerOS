d4bdf0b76c76aa45f60f5f3e3dab0e170b4bd301
Prankiman <kolsmyrarvid@gmail.com>
2023-01-03 21:07:51 +0100

    commit

--- os_tinykernel_project/io.c
+++ os_tinykernel_project/io.c
@@ -42,6 +42,26 @@ u16 inw(u16 port)
     return ret;
 }
 
+void outl(u16 port, u32 val)
+{
+    __asm__ __volatile__ ("outl %0, %1" : : "a"(val), "Nd"(port) );
+    /* There's an outb %al, $imm8  encoding, for compile-time constant port numbers that fit in 8b.  (N constraint).
+     * Wider immediate constants would be truncated at assemble-time (e.g. "i" constraint).
+     * The  outb  %al, %dx  encoding is the only option for all other cases.
+     * %1 expands to %dx because  port  is a uint16_t.  %w1 could be used if we had the port number a wider C type */
+}
+
+
+u32 inl(u16 port)
+{
+    u16 ret;
+
+    __asm__ __volatile__ ( "inl %1, %k0"
+                   : "=a"(ret)
+                   : "Nd"(port) ); //k modifier to select 32-bit sub register
+    return ret;
+}
+
 void io_wait(void)
 {
     outb(0x80, 0);//basically a no-op
--- os_tinykernel_project/io.h
+++ os_tinykernel_project/io.h
@@ -11,6 +11,11 @@ void outw(u16 port, u16 val);
 
 u16 inw(u16 port);
 
+void outl(u16 port, u32 val);
+
+u32 inl(u16 port);
+
+
 void io_wait(void);
 
 
